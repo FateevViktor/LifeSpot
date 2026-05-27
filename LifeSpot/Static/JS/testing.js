e172766df2ebaf481e76a4d79f3e07e817bd2246
FateevVV <FateevVV@yandex.ru>
2026-05-27 17:04:34 +0300

    сайт LifeSpot

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,28 @@
+﻿// для получения пользовательского ввода самое простое - воспользоваться конструкцией prompt()
+let userName = prompt("Пожалуйста, введите ваше имя");
+alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
+
+/*
+* Функция для фильтрации контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+*
+* */
+function filterContent(userInput) {
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
